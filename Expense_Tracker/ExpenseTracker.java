5554b5b0065ad410b2fbca82c474bc90fed72749
Esteban <112445347+Mesa112@users.noreply.github.com>
2023-11-25 12:44:18 -0500

    Add files via upload

---
+++ Expense_Tracker/ExpenseTracker.java
@@ -0,0 +1,60 @@
+package Expense_Tracker;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import java.util.Scanner;
+
+public class ExpenseTracker {
+    private HashMap<String, List<Transaction>> transactions;
+    private double balance; // Added to keep track of the current balance
+
+    public ExpenseTracker() {
+        transactions = new HashMap<>();
+        balance = 0; // Initial balance can be set to 0 or any other starting value
+    }
+
+    public void addTransaction(String type, double amount, String category, String date) {
+        // Update balance based on transaction type
+        if ("Income".equalsIgnoreCase(type)) {
+            balance += amount;
+        } else if ("Expense".equalsIgnoreCase(type)) {
+            balance -= amount;
+        }
+
+        Transaction newTransaction = new Transaction(type, amount, category, date);
+        if (!transactions.containsKey(category)) {
+            transactions.put(category, new ArrayList<>());
+        }
+        transactions.get(category).add(newTransaction);
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+ // In your ExpenseTracker class
+
+    public String getSummary() {
+        StringBuilder summaryBuilder = new StringBuilder();
+        double totalIncome = 0;
+        double totalExpense = 0;
+
+        for (List<Transaction> transList : transactions.values()) {
+            for (Transaction trans : transList) {
+                if ("Income".equalsIgnoreCase(trans.getType())) {
+                    totalIncome += trans.getAmount();
+                } else if ("Expense".equalsIgnoreCase(trans.getType())) {
+                    totalExpense += trans.getAmount();
+                }
+            }
+        }
+
+        summaryBuilder.append("----- Summary -----\n");
+        summaryBuilder.append("Total Income: $").append(String.format("%.2f", totalIncome)).append("\n");
+        summaryBuilder.append("Total Expense: $").append(String.format("%.2f", totalExpense)).append("\n");
+        summaryBuilder.append("Net Flow: $").append(String.format("%.2f", (totalIncome - totalExpense))).append("\n");
+        summaryBuilder.append("-------------------\n");
+
+        return summaryBuilder.toString();
+    }
+}
